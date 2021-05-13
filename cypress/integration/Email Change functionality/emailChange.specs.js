eccccf8eceee6a6676b4e25cfe24af8818f26022
sidd_oo <sahoosiddharth3@gmail.com>
2021-05-14 04:38:12 +0530

    email Change tests modified and email restore test added

--- cypress/integration/Email Change functionality/emailChange.specs.js
+++ cypress/integration/Email Change functionality/emailChange.specs.js
@@ -1,65 +1,42 @@
  /// <reference types="cypress" />
  import {emailTab} from '../../utils/changeEmailTab'
  describe("NeetoAuth Email Change Functionality", () => {
-   before(() => {
-     emailTab();
-       });
-    
-  it("Change email and verify by login with new email, old email should prompt error msg while login", () => {
+   
+  it("Change email and verify that the old email can't be used for login", () => {
+    emailTab();
+    cy.fixture("credentials").then((user)=>{
+        cy.emailChange(user.new.email, user.old.password);
+        cy.login(user.correct.email,user.correct.password);
+        cy.get('[data-cy=login-submit-button]').click();
+        cy.get('[data-cy=toastr-message-container]').should('have.text','Something went wrong.');
+        cy.restoreEmail(user.new.email,user.old.password);
+    })
+      
+  });
+
+  it("Change email and verify that only current email can be used for login", () => {
+    emailTab();
       cy.fixture("credentials").then((user)=>{
-        cy.get('[data-cy=change-email-new-email-text-field]').type(user.new.email);
-        cy.get('[data-cy=change-email-current-password-text-field]').type(user.old.password);
-        cy.get('[data-cy=change-email-submit-button]').click();
-        cy.get('[data-cy=toastr-message-container]').should('have.text','Email successfully changed!')
-        cy.get('[data-cy="profile-settings-logout-button"]').click();
-      })
-
-      cy.fixture("credentials").then((user) => {
-      cy.login(user.correct.email,user.correct.password);
-      })
-      cy.get('[data-cy=login-submit-button]').click();
-      cy.get('[data-cy=toastr-message-container]').should('have.text','Something went wrong.');
-
-      cy.fixture("credentials").then((user) => {
+        cy.emailChange(user.new.email, user.old.password);
         cy.login(user.new.email,user.correct.password);
+        cy.loginSubmit();
+        cy.get('[data-cy=heading]').should('have.text',"Profile Settings");
+        cy.logout();
+        cy.restoreEmail(user.new.email,user.old.password);
       })
-      cy.get('[data-cy=login-submit-button]').click();
-      cy.get('[data-cy=heading]').should('have.text',"Profile Settings");
   });
 
   it("Restore the old email and verfify",()=>{
-      cy.visit('https://spinkart.neetoauth.net')
-      cy.fixture("credentials").then((user) => {
-        cy.login(user.new.email,user.old.password);
-      })
-      cy.get('[data-cy=login-submit-button]').click();
-
-      cy.get('.bp3-popover-target > .relative').click();
-      cy.get('[data-cy="nav-profile-link"]').click();
-
-      cy.get('[data-cy=profile-settings-change-email-tab]').click();
-
-      cy.fixture("credentials").then((user)=>{
-        cy.get('[data-cy=change-email-new-email-text-field]').type(user.old.email);
-        cy.get('[data-cy=change-email-current-password-text-field]').type(user.old.password);
-        cy.get('[data-cy=change-email-submit-button]').click();
-        cy.get('[data-cy=toastr-message-container]').should('have.text','Email successfully changed!')
-        cy.get('[data-cy="profile-settings-logout-button"]').click();
-      })
-
-      cy.fixture("credentials").then((user) => {
-        cy.login(user.new.email,user.old.password);
-      })
-      cy.get('[data-cy=login-submit-button]').click();
-      cy.get('[data-cy=toastr-message-container]').should('have.text','Something went wrong.');
-
-      cy.fixture("credentials").then((user) => {
+    emailTab();
+    cy.fixture("credentials").then((user) => {
+        cy.emailChange(user.new.email, user.old.password);
+        cy.restoreEmail(user.new.email,user.old.password);
+        cy.visit('https://spinkart.neetoauth.net')
         cy.login(user.old.email,user.old.password);
+        cy.loginSubmit();
+        cy.get('[data-cy=heading]').should('have.text',"Profile Settings");
+        cy.logout();
       })
-      cy.get('[data-cy=login-submit-button]').click();
-      cy.get('[data-cy=heading]').should('have.text',"Profile Settings");
   })
-
-
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -83,3 +83,30 @@ Cypress.Commands.add('resetPassword',(currentPwd, newPwd)=>{
     cy.get('[data-cy=toastr-message-container]').should('have.text','Password successfully changed!');
     cy.logout();
 })
+
+Cypress.Commands.add('emailChange',(newEmail, currentPwd)=>{
+        cy.get('[data-cy=change-email-new-email-text-field]').type(newEmail);
+        cy.get('[data-cy=change-email-current-password-text-field]').type(currentPwd);
+        cy.get('[data-cy=change-email-submit-button]').click();
+        cy.get('[data-cy=toastr-message-container]').should('have.text','Email successfully changed!')
+        cy.logout();
+})
+
+
+Cypress.Commands.add('restoreEmail',(newEmail, currentPwd)=>{
+    cy.visit('https://spinkart.neetoauth.net')
+    cy.fixture("credentials").then((user) => {
+        cy.login(newEmail,currentPwd);
+    })
+    cy.loginSubmit();
+    cy.get('.bp3-popover-target > .relative').click();
+    cy.get('[data-cy="nav-profile-link"]').click();
+    
+    cy.get('[data-cy=profile-settings-change-email-tab]').click();
+    
+    cy.fixture("credentials").then((user)=>{
+        cy.emailChange(user.old.email,user.old.password)
+    })
+})    
+
+
